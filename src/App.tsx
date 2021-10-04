06aa20f32da288a50bc52ef9b65b0def1dd96af1
reversee <rare7989@gmail.com>
2021-10-05 00:04:09 +0200

    production

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Countdown from "react-countdown";
+import {Helmet} from "react-helmet";
+
+const TITLE = 'odliczanko'
+
+var countDownDate = new Date("Oct 15, 2021 00:00:00").getTime();
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          pozostało (d/h/m/s)
+        </p>
+          <div className="rainbow">
+              <Countdown date={countDownDate}>
+                  <a href={"https://youtu.be/dQw4w9WgXcQ"}>click meeee</a>
+              </Countdown>
+          </div>
+          <div>
+              <h6>ciekawe co sie wtedy stanie...</h6>
+          </div>
+      </header>
+        <Helmet>
+            <title>{ TITLE }</title>
+        </Helmet>
+    </div>
+  );
+}
+
+export default App;
